4400f53ae880ac685946e3e160c31c647ea9ee58
Abhishek Gupta <abhishek.gupta13@nagarro.com>
2022-07-05 14:51:18 +0530

    feat: Implementing routing

--- src/App.js
+++ src/App.js
@@ -1,44 +1,51 @@
 import "./App.css";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
+const ChidldDiv = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${(props) => (props.primary ? "palevioletred" : "white")};
+  color: ${(props) => (props.primary ? "white" : "palevioletred")};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const ParentDiv = styled.div`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
 function App() {
-  const ChidldDiv = styled.button`
-    /* Adapt the colors based on primary prop */
-    background: ${(props) => (props.primary ? "palevioletred" : "white")};
-    color: ${(props) => (props.primary ? "white" : "palevioletred")};
-
-    font-size: 1em;
-    margin: 1em;
-    padding: 0.25em 1em;
-    border: 2px solid palevioletred;
-    border-radius: 3px;
-  `;
-
-  const ParentDiv = styled.div`
-    height: 100vh;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-  `;
-
-  const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: palevioletred;
-  `;
-
-  const Wrapper = styled.section`
-    padding: 4em;
-    background: papayawhip;
-  `;
+  let navigate = useNavigate();
 
   return (
     <ParentDiv>
       <Wrapper>
         <Title>THE_SOCIAL_PROJECT</Title>
       </Wrapper>
-      <ChidldDiv primary>NEXT</ChidldDiv>
+      <ChidldDiv primary onClick={() => navigate("/quiz")}>
+        NEXT
+      </ChidldDiv>
     </ParentDiv>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import BoxContainer from "./Container/BoxContainer";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/quiz" element={<BoxContainer />} />
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
