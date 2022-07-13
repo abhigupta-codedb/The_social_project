db258319ea2d9f6b76191abf540cbd0cfb846325
Abhishek Gupta <abhishek.gupta13@nagarro.com>
2022-07-14 00:40:55 +0530

    feat: Question addition feature

--- src/Components/AddQuestions.jsx
+++ src/Components/AddQuestions.jsx
@@ -1,5 +1,22 @@
 import React from "react";
 import styled from "styled-components";
+import { useState } from "react";
+import { quiz } from "../Helper/quiz";
+
+const Button = styled.button`
+  background: white;
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em 0em;
+  padding: 0.25em 1em;
+  border: 2px solid white;
+  border-radius: 3px;
+  &:hover {
+    cursor: pointer;
+    background: papayawhip;
+    color: palevioletred;
+  }
+`;
 
 const Parent = styled.div`
   height: 90vh;
@@ -9,6 +26,10 @@ const Parent = styled.div`
   align-items: center;
 `;
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 const Wrapper = styled.div`
   width: 50vw;
   height: 50vh;
@@ -20,13 +41,13 @@ const Form = styled(Wrapper)`
   border: none;
   background: none;
   display: flex;
-  align-items: center;
   flex-direction: column;
+  align-items: center;
   justify-content: center;
 `;
 
 const TextArea = styled.textarea`
-  width: 20vw;
+  width: 50%;
   height: 5vh;
   border: 2px solid palevioletred;
   border-radius: 3px;
@@ -36,59 +57,135 @@ const Field = styled.div`
   margin: 5px;
   display: flex;
   flex-direction: row;
+  width: 100%;
 `;
 
-const Label = styled.span`
-  width: 15vw;
-  height: 5vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Text = styled.h3`
-  font-size: 0.75em;
+const Text = styled.span`
+  font-size: 1em;
   text-align: center;
   color: palevioletred;
+  display: inline-block;
+  width: 30%;
+`;
+
+const Dropdown = () => (
+  <select>
+    <option value={1}>1</option>
+    <option value={2}>2</option>
+    <option value={3}>3</option>
+    <option value={4}>4</option>
+  </select>
+);
+
+const DropdownWrap = styled.span`
+  margin: 5px;
 `;
 
 const AddQuestions = () => {
+  //TODO: put all state update inside useDispatch
+  const [getQues, setQues] = useState("");
+  const [getop1, setop1] = useState("");
+  const [getop2, setop2] = useState("");
+  const [getop3, setop3] = useState("");
+  const [getop4, setop4] = useState("");
+
+  const submitHandler = () => {
+    if (!getQues || !getop1 || !getop2 || !getop3 || !getop4) {
+      alert("Empty field found");
+    } else {
+      const newObj = {
+        question: getQues,
+        options: [
+          {
+            Text: getop1,
+            value: 4,
+          },
+          {
+            Text: getop2,
+            value: 3,
+          },
+          {
+            Text: getop3,
+            value: 2,
+          },
+          {
+            Text: getop4,
+            value: 1,
+          },
+        ],
+      };
+      quiz.push(newObj);
+      clearHandler();
+    }
+  };
+  const clearHandler = () => {
+    setQues("");
+    setop1("");
+    setop2("");
+    setop3("");
+    setop4("");
+  };
+
+  //TODO: Optmize dropdown code
   return (
     <Parent>
-      <Wrapper>
-        <Form>
-          <Field>
-            <Label>
+      <Container>
+        <Wrapper>
+          <Form>
+            <Field>
               <Text>Add Question </Text>
-            </Label>
-            <TextArea />
-          </Field>
-          <Field>
-            <Label>
+              <TextArea
+                value={getQues}
+                onChange={(e) => setQues(e.target.value)}
+              />
+            </Field>
+            <Field>
               <Text>Option 1 </Text>
-            </Label>
-            <TextArea />
-          </Field>
-          <Field>
-            <Label>
+              <TextArea
+                value={getop1}
+                onChange={(e) => setop1(e.target.value)}
+              />
+              <DropdownWrap>
+                <Dropdown />
+              </DropdownWrap>
+            </Field>
+            <Field>
               <Text>Option 2 </Text>
-            </Label>
-            <TextArea />
-          </Field>
-          <Field>
-            <Label>
+              <TextArea
+                value={getop2}
+                onChange={(e) => setop2(e.target.value)}
+              />
+              <DropdownWrap>
+                <Dropdown />
+              </DropdownWrap>
+            </Field>
+            <Field>
               <Text>Option 3 </Text>
-            </Label>
-            <TextArea />
-          </Field>
-          <Field>
-            <Label>
+              <TextArea
+                value={getop3}
+                onChange={(e) => setop3(e.target.value)}
+              />
+              <DropdownWrap>
+                <Dropdown />
+              </DropdownWrap>
+            </Field>
+            <Field>
               <Text>Option 4 </Text>
-            </Label>
-            <TextArea />
-          </Field>
-        </Form>
-      </Wrapper>
+              <TextArea
+                value={getop4}
+                onChange={(e) => setop4(e.target.value)}
+              />
+              <DropdownWrap>
+                <Dropdown />
+              </DropdownWrap>
+            </Field>
+          </Form>
+        </Wrapper>
+        <div>
+          <Button onClick={submitHandler}>Submit</Button>{" "}
+          <Button onClick={clearHandler}>Clear all</Button>
+        </div>
+      </Container>
     </Parent>
   );
 };
