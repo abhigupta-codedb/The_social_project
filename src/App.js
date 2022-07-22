9c2fab2dac97d13bedfd2a639cb4c4fc18c3e6d7
Abhishek Gupta <abhishek.gupta13@nagarro.com>
2022-07-22 16:49:56 +0530

    feat: improving login/routing function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route, useNavigate, useLocation } from "react-router-dom";
 import BoxContainer from "./Container/BoxContainer";
 import Home from "./Components/Home";
 import Questions from "./Components/Questions";
@@ -8,6 +8,7 @@ import { useDispatch } from "react-redux/es/exports";
 import { loadQuestions } from "./Slices/Group1";
 import { quiz } from "./Helper/quiz";
 import Login from "./Components/Login";
+import Signup from "./Components/Signup";
 
 let dispatchFired = false;
 
@@ -15,6 +16,7 @@ function App() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const [isLogin, setLogin] = useState(false);
+  const { pathname } = useLocation();
 
   useEffect(() => {
     if (!dispatchFired) {
@@ -24,10 +26,13 @@ function App() {
   }, [dispatch]);
 
   useEffect(() => {
-    if (!isLogin) {
+    if (
+      !isLogin &&
+      ["/quiz", "/Questions", "/home"].includes(pathname.toString())
+    ) {
       navigate("/");
     }
-  }, [isLogin, navigate]);
+  }, [isLogin, navigate, pathname]);
 
   return (
     <Routes>
@@ -38,6 +43,7 @@ function App() {
       <Route path="/quiz" element={<BoxContainer />} />
       <Route path="/Questions" element={<Questions />} />
       <Route path="/" element={<Login setLogin={setLogin} />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
   );
 }
